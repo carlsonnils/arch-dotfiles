3d9a24edd8cb28cdfd496037c306623f1ef9acb6
nils <carlsonnils@gmail.com>
2026-06-10 11:28:51 +0000

    script to remove configs

---
+++ clean_up.sh
@@ -0,0 +1,13 @@
+#!/user/bin/env bash
+
+CONFIG_DIR="$HOME/.config"
+
+rm -r $CONFIG_DIR/hypr
+rm -r $CONFIG_DIR/waybar
+rm -r $CONFIG_DIR/hypr
+rm -r $CONFIG_DIR/hypr
+rm -r $CONFIG_DIR/hypr
+rm -r $CONFIG_DIR/alacritty
+rm -r $CONFIG_DIR/mako
+rm -r $CONFIG_DIR/yazi
+rm -r $CONFIG_DIR/git
